0bde0c3c37fa10816ef6ca653d96675d452edb06
vermaarpit14 <vermaarpit253@gmail.com>
2025-12-28 01:08:58 +0530

    development config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,24 @@
 import express from 'express';
+import path from 'path';
 import { ENV } from "./lib/env.js";
 
 const app = express();
+const __dirname = path.resolve();
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Server is running fine' });
+app.get('/health', (req, res) => {
+    res.status(200).json({ message: 'Server is running fine on Health' });
 });
+
+app.get('/books', (req, res) => {
+    res.status(200).json({ message: 'Server is running fine on Books' });
+});
+
+if(ENV.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get('/{*any}', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+    });
+}
 
 app.listen(ENV.PORT, () => {
     console.log(`Server is running on port ${ENV.PORT}`);
